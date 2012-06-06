d1cd972baccf8144524ef7b9e4cf76478ca74e3a
tpluscode <tomasz@t-code.pl>
2012-06-06 10:10:15 +0200

    testing DirectMappingR2RMLBuilder#R2RMLGraph

---
+++ src/TCode.r2rml4net.Mapping/DirectMappingR2RMLBuilder.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCode.r2rml4net.Mapping
+{
+    /// <summary>
+    /// Builds a R2RML graph from a relational database's schema
+    /// </summary>
+    public class DirectMappingR2RMLBuilder
+    {
+        private RDB.IDatabaseMetadataProvider _databaseMetadataProvider;
+
+        /// <summary>
+        /// Creates <see cref="DirectMappingR2RMLBuilder"/> which will read RDB metadata using <see cref="RDB.IDatabaseMetadataProvider"/>
+        /// </summary>
+        public DirectMappingR2RMLBuilder(RDB.IDatabaseMetadataProvider databaseMetadataProvider)
+        {
+            this._databaseMetadataProvider = databaseMetadataProvider;
+        }
+
+        /// <summary>
+        /// Returns an R2RML graph generated for direct mapping
+        /// </summary>
+        public VDS.RDF.IGraph R2RMLGraph
+        {
+            get
+            {
+                return null;
+            }
+        }
+    }
+}
