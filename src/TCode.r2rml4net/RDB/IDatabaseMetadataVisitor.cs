1c0f12bfc2f907ae843e530449cad43205beedb6
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-06-06 10:32:56 +0200

    visitor pattern for iterating over database metadata

---
+++ src/TCode.r2rml4net/RDB/IDatabaseMetadataVisitor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCode.r2rml4net.RDB
+{
+    public interface IDatabaseMetadataVisitor
+    {
+        void Visit(TableCollection tables);
+        void Visit(TableMetadata tables);
+        void Visit(ColumnMetadata tables);
+    }
+}
---
+++ src/TCode.r2rml4net/RDB/TableMetadata.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace TCode.r2rml4net.RDB
+{
+    public class TableMetadata
+    {
+        public IEnumerable<ColumnMetadata> Columns { get; internal set; }
+    }
+}
