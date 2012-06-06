6fc2dedaf88d587bfc7dcd54aa2836a64f1e23ac
tpluscode <tomasz@t-code.pl>
2012-06-06 14:39:31 +0200

    nunit runner

---
+++ src/TCode.r2rml4net.Tests/Program.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+
+namespace TCode.r2rml4net.Tests
+{
+    public class Program
+    {
+        static int Main()
+        {
+            return NUnit.Gui.AppEntry.Main(new string[] { Assembly.GetExecutingAssembly().Location });
+        }
+    }
+}
