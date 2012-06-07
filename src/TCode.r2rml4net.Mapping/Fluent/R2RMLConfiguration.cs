bb911c7a611b230a8b7c2f50385074f1ed0caf2e
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-06-07 19:44:15 +0200

    starting fluent R2RML with tests

---
+++ src/TCode.r2rml4net.Mapping/Fluent/R2RMLConfiguration.cs
@@ -0,0 +1,18 @@
+﻿namespace TCode.r2rml4net.Mapping.Fluent
+{
+    /// <summary>
+    /// Entrypoint to all of R2RML fluent configuration interfaces
+    /// </summary>
+    public class R2RMLConfiguration
+    {
+        public ITriplesMapConfiguration CreateTriplesMapFromTable(string tablename)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public ITriplesMapConfiguration CreateTriplesMapFromR2RMLView(string sqlQuery)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
--- src/TCode.r2rml4net.Mapping/R2RMLMappings.cs
+++ src/TCode.r2rml4net.Mapping/R2RMLMappings.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using TCode.r2rml4net.Mapping.Fluent;
 using VDS.RDF;
 using TCode.r2rml4net.RDB;
 
