d9ec88a9f3e171cc1f26798d3d14fb2d2c5cb8a2
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-06-07 22:05:45 +0200

    test for asserting TripleMap triples

--- src/TCode.r2rml4net.Mapping.Tests/DotnetrdfR2RMLConfigurationTests.cs
+++ src/TCode.r2rml4net.Mapping.Tests/DotnetrdfR2RMLConfigurationTests.cs
@@ -1,21 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using Moq;
 using NUnit.Framework;
 using TCode.r2rml4net.Mapping.Fluent;
 using TCode.r2rml4net.Mapping.Fluent.Dotnetrdf;
+using VDS.RDF;
 
 namespace TCode.r2rml4net.Mapping.Tests
 {
     [TestFixture]
     public class DotnetrdfR2RMLConfigurationTests
     {
+        private Mock<Graph> _graph;
         private R2RMLConfiguration _configuration;
 
         [SetUp]
         public void Setup()
         {
-            _configuration = new R2RMLConfiguration();
+            _graph = new Mock<Graph>
+                         {
+                             CallBase = true
+                         };
+            _configuration = new R2RMLConfiguration();// {R2RMLMappings = _graph.Object};
         }
 
         [TestCase(1)]
@@ -91,5 +98,128 @@ namespace TCode.r2rml4net.Mapping.Tests
             Assert.IsInstanceOf<TriplesMapConfiguration>(configuration);
             Assert.IsNotNull(configuration);
         }
+
+        [Test]
+        public void CreatingTriplesMapFromTableNameAssertsTriplesInGraph()
+        {
+            // given
+            const string tablename = "TableName";
+            string triplesMapUri = string.Format("{0}{1}TriplesMap", _configuration.R2RMLMappings.BaseUri, tablename);
+
+            // when
+            _configuration.CreateTriplesMapFromTable(tablename);
+
+            // then
+            //_graph.Verify(g => g.CreateUriNode("rr:TriplesMap"), Times.Once());
+            //_graph.Verify(g => g.CreateUriNode("rdf:Type"), Times.Once());
+            //_graph.Verify(g => g.CreateUriNode("rr:logicalTable"), Times.Once());
+            //_graph.Verify(g => g.CreateUriNode("rr:tableName"), Times.Once());
+            //_graph.Verify(g => g.CreateLiteralNode(tablename), Times.Once());
+            //_graph.Verify(g => g.CreateBlankNode(), Times.Once());
+
+            AssertTripleAssertion(triplesMapUri, RdfType, RrTriplesMapClass);
+            //AssertTripleAssertionWithBlankNodeObject(triplesMapUri, RrLogicalTableProperty);
+            //AssertTripleAssertionWithBlankSubjectAndLiteralNode(RrTableNameProperty, tablename);
+        }
+
+        [Test]
+        public void ConfigurationBuilderCreatedWithAnEmptyGraph()
+        {
+            Assert.IsTrue(_configuration.R2RMLMappings.IsEmpty);
+        }
+
+        [Test]
+        public void ConfigurationBuilderCreatedWithGraphWithDefaultNamespaces()
+        {
+            Assert.IsTrue(_configuration.R2RMLMappings.NamespaceMap.HasNamespace("rr"));
+            Assert.AreEqual("http://www.w3.org/ns/r2rml#", _configuration.R2RMLMappings.NamespaceMap.GetNamespaceUri("rr").ToString());
+
+            Assert.IsTrue(_configuration.R2RMLMappings.NamespaceMap.HasNamespace("rdf"));
+            Assert.AreEqual("http://www.w3.org/1999/02/22-rdf-syntax-ns#", _configuration.R2RMLMappings.NamespaceMap.GetNamespaceUri("rdf").ToString());
+        }
+
+        [Test]
+        public void ConfigurationBuilderConstructedWithDefaultBaseUri()
+        {
+            Assert.AreEqual(R2RMLConfiguration.DefaultBaseUri, _configuration.R2RMLMappings.BaseUri);
+        }
+
+        [Test]
+        public void ConfigurationBuilderCanBeConstructedWithChangedDefaultBaseUri()
+        {
+            Uri baseUri = new Uri("http://this.is.test.com/rdf/");
+
+            _configuration = new R2RMLConfiguration(baseUri);
+
+            Assert.AreEqual(baseUri, _configuration.R2RMLMappings.BaseUri);
+        }
+
+        #region Assertion helper methods
+
+        private void AssertGraphHasNode(string uri)
+        {
+            Assert.IsNotNull(_configuration.R2RMLMappings.GetUriNode(new Uri(uri)), string.Format("Node <{0}> not found in graph {1}", uri, _configuration.R2RMLMappings));
+        }
+
+        private void AssertTripleAssertion(string subjectUri, string predicateUri, string objectUri)
+        {
+            AssertGraphHasNode(subjectUri);
+            AssertGraphHasNode(predicateUri);
+            AssertGraphHasNode(objectUri);
+
+            Assert.IsTrue(_configuration.R2RMLMappings.ContainsTriple(new Triple(
+                _configuration.R2RMLMappings.GetUriNode(new Uri(subjectUri)),
+                _configuration.R2RMLMappings.GetUriNode(new Uri(predicateUri)),
+                _configuration.R2RMLMappings.GetUriNode(new Uri(objectUri))
+                )), string.Format("Triple <{0}> => <{1}> => <{2}> not found in graph", subjectUri, predicateUri, objectUri));
+        }
+
+        private void AssertTripleAssertionWithBlankNodeObject(string subjectUri, string predicateUri)
+        {
+            _graph.Verify(g => g.Assert(
+                It.Is<UriNode>(node => node.Uri.ToString() == subjectUri),
+                It.Is<UriNode>(node => node.Uri.ToString() == predicateUri),
+                It.IsAny<BlankNode>()
+                ));
+        }
+
+        private void AssertTripleAssertionWithBlankNodeSubject(string predicateUri, string objectUri)
+        {
+            _graph.Verify(g => g.Assert(
+                It.IsAny<BlankNode>(),
+                It.Is<UriNode>(node => node.Uri.ToString() == predicateUri),
+                It.Is<UriNode>(node => node.Uri.ToString() == objectUri)
+                ));
+        }
+
+        private void AssertTripleAssertionWithLiteralNode(string subjectUri, string predicateUri, string literalValue)
+        {
+            _graph.Verify(g => g.Assert(
+                It.Is<UriNode>(node => node.Uri.ToString() == subjectUri),
+                It.Is<UriNode>(node => node.Uri.ToString() == predicateUri),
+                It.Is<LiteralNode>(node => node.Value == literalValue)
+                ));
+        }
+
+        private void AssertTripleAssertionWithBlankSubjectAndLiteralNode(string predicateUri, string literalValue)
+        {
+            _graph.Verify(g => g.Assert(
+                It.IsAny<BlankNode>(),
+                It.Is<UriNode>(node => node.Uri.ToString() == predicateUri),
+                It.Is<LiteralNode>(node => node.Value == literalValue)
+                ));
+        }
+
+        #endregion
+
+        #region Uri constants
+
+        private const string RrTriplesMapClass = "http://www.w3.org/ns/r2rml#TriplesMap";
+        private const string RrLogicalTableProperty = "http://www.w3.org/ns/r2rml#logicalTable";
+        private const string RrTableNameProperty = "http://www.w3.org/ns/r2rml#tableName";
+
+        private const string RdfType = "http://www.w3.org/1999/02/22-rdf-syntax-ns#type";
+
+        #endregion
     }
 }
--- src/TCode.r2rml4net.Mapping/Fluent/Dotnetrdf/R2RMLConfiguration.cs
+++ src/TCode.r2rml4net.Mapping/Fluent/Dotnetrdf/R2RMLConfiguration.cs
@@ -1,13 +1,50 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using VDS.RDF;
 
 namespace TCode.r2rml4net.Mapping.Fluent.Dotnetrdf
 {
     class R2RMLConfiguration : IR2RMLConfiguration
     {
-        public IGraph R2RMLMappings { get; internal set; }
+        private readonly Uri _baseUri;
+
+        internal static Uri DefaultBaseUri{get
+        {
+            return new Uri("http://r2rml.net/mappings#");
+        }}
+
+        private IGraph _R2RMLMappings;
+        internal IGraph R2RMLMappings
+        {
+            get
+            {
+                if (_R2RMLMappings == null)
+                {
+                    _R2RMLMappings = new Graph { BaseUri = _baseUri };
+
+                    _R2RMLMappings.NamespaceMap.AddNamespace("rr", new Uri("http://www.w3.org/ns/r2rml#"));
+                }
+
+                return _R2RMLMappings;
+            }
+        }
+
+        protected Uri MappingBaseUri
+        {
+            get { return new Uri("http://example.com/"); }
+        }
 
         readonly IList<ITriplesMapConfiguration> _triplesMaps = new List<ITriplesMapConfiguration>();
+
+        public R2RMLConfiguration(Uri baseUri):this()
+        {
+            _baseUri = baseUri;
+        }
+
+        public R2RMLConfiguration()
+        {
+            _baseUri = DefaultBaseUri;
+        }
 
         public ITriplesMapConfiguration CreateTriplesMapFromTable(string tablename)
         {
@@ -16,9 +53,25 @@ namespace TCode.r2rml4net.Mapping.Fluent.Dotnetrdf
             if (string.IsNullOrWhiteSpace(tablename))
                 throw new System.ArgumentOutOfRangeException("tablename");
 
+            AssertTriples(tablename);
+
             var triplesMapConfiguration = new TriplesMapConfiguration(R2RMLMappings);
             _triplesMaps.Add(triplesMapConfiguration);
             return triplesMapConfiguration;
+        }
+
+        private void AssertTriples(string tablename)
+        {
+            var type = R2RMLMappings.CreateUriNode("rdf:type");
+            var tripleMap = R2RMLMappings.CreateUriNode("rr:TriplesMap");
+            var logicalTable = R2RMLMappings.CreateUriNode("rr:logicalTable");
+            var tableName = R2RMLMappings.CreateUriNode("rr:tableName");
+            var tableNameLiteral = R2RMLMappings.CreateLiteralNode(tablename);
+            var tableDefinition = R2RMLMappings.CreateBlankNode();
+
+            R2RMLMappings.Assert(tripleMap, type, tripleMap);
+            R2RMLMappings.Assert(tripleMap, logicalTable, tableDefinition);
+            R2RMLMappings.Assert(tableDefinition, tableName, tableNameLiteral);
         }
 
         public ITriplesMapFromR2RMLViewConfiguration CreateTriplesMapFromR2RMLView(string sqlQuery)
