8db6508e984f7d591de7e2577ea788098c77045a
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-06-07 20:25:30 +0200

    creation tests pass

---
+++ src/TCode.r2rml4net.Mapping/Fluent/Dotnetrdf/R2RMLConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using VDS.RDF;
+
+namespace TCode.r2rml4net.Mapping.Fluent.Dotnetrdf
+{
+    class R2RMLConfiguration : IR2RMLConfiguration
+    {
+        public IGraph R2RMLMappings { get; internal set; }
+
+        readonly IList<ITriplesMapConfiguration> _triplesMaps = new List<ITriplesMapConfiguration>();
+
+        public ITriplesMapConfiguration CreateTriplesMapFromTable(string tablename)
+        {
+            var triplesMapConfiguration = new TriplesMapConfiguration(R2RMLMappings);
+            _triplesMaps.Add(triplesMapConfiguration);
+            return triplesMapConfiguration;
+        }
+
+        public ITriplesMapFromR2RMLViewConfiguration CreateTriplesMapFromR2RMLView(string sqlQuery)
+        {
+            var triplesMapConfiguration = new TriplesMapConfiguration(R2RMLMappings);
+            _triplesMaps.Add(triplesMapConfiguration);
+            return triplesMapConfiguration;
+        }
+    }
+}
