95e84df7f2fef73787ce8e3b9bce9f9534bd5b1c
tpluscode <tomasz@t-code.pl>
2012-06-08 13:27:19 +0200

    the rest of missing comments

--- src/TCode.r2rml4net/RDB/IDatabaseMetadata.cs
+++ src/TCode.r2rml4net/RDB/IDatabaseMetadata.cs
@@ -1,8 +1,17 @@
 ﻿namespace TCode.r2rml4net.RDB
 {
+    /// <summary>
+    /// Interface for reading and accessing structure of a relational database such as MSSQL, MySQL, Oracle etc.
+    /// </summary>
     public interface IDatabaseMetadata
     {
+        /// <summary>
+        /// Reads metadata from the relational database
+        /// </summary>
         void ReadMetadata();
+        /// <summary>
+        /// Collecion of tables contained by the current database
+        /// </summary>
         TableCollection Tables { get; }
     }
 }
--- src/TCode.r2rml4net/RDB/TableCollection.cs
+++ src/TCode.r2rml4net/RDB/TableCollection.cs
@@ -5,8 +5,14 @@ using System.Text;
 
 namespace TCode.r2rml4net.RDB
 {
+    /// <summary>
+    /// Implementation od <see cref="List{T}"/> and <see cref="IVistitable{T}"/> representing a group of tables
+    /// </summary>
     public class TableCollection : List<TableMetadata>, IVistitable<IDatabaseMetadataVisitor>
     {
+        /// <summary>
+        /// Visits self and all contained tables
+        /// </summary>
         public void Accept(IDatabaseMetadataVisitor visitor)
         {
             visitor.Visit(this);
