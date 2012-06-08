1a6ff37b92b2a3b4145c7a7628448c886f5b3e22
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-06-08 13:18:41 +0200

    comments and Uri in InvalidTriplesMapException

--- src/TCode.r2rml4net.Mapping/InvalidTriplesMapException.cs
+++ src/TCode.r2rml4net.Mapping/InvalidTriplesMapException.cs
@@ -5,11 +5,33 @@ using System.Text;
 
 namespace TCode.r2rml4net.Mapping
 {
+    /// <summary>
+    /// Represents a validation error in Triples Map structure
+    /// </summary>
     public class InvalidTriplesMapException : Exception
     {
-        public InvalidTriplesMapException(string message)
+        /// <summary>
+        /// Uri of node where error occured
+        /// </summary>
+        public Uri Uri { get; private set; }
+
+        /// <summary>
+        /// Creates an instance of <see cref="InvalidTriplesMapException"/> with a message
+        /// </summary>
+        public InvalidTriplesMapException(string message, Uri errorneousNodeUri)
             : base(message)
         {
+        }
+
+        /// <summary>
+        /// Returns a message passed to the constructor and Uri of node where error occured
+        /// </summary>
+        public override string Message
+        {
+            get
+            {
+                return string.Format("{0}. Error in node {1}", base.Message, Uri);
+            }
         }
     }
 }
