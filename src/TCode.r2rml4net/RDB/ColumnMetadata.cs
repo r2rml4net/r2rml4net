0d09ec93df68b2a8e35e5bd35a4e0391538d1a73
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-06-08 13:27:19 +0200

    the rest of missing comments

--- src/TCode.r2rml4net/RDB/ColumnMetadata.cs
+++ src/TCode.r2rml4net/RDB/ColumnMetadata.cs
@@ -6,12 +6,27 @@ using System.Data;
 
 namespace TCode.r2rml4net.RDB
 {
+    /// <summary>
+    /// Represents a column from a relational database
+    /// </summary>
     public class ColumnMetadata : IVistitable<IDatabaseMetadataVisitor>
     {
+        /// <summary>
+        /// Column name
+        /// </summary>
         public string Name { get; internal set; }
+        /// <summary>
+        /// Column <see cref="DbType"/>
+        /// </summary>
         public DbType Type { get; internal set; }
+        /// <summary>
+        /// The <see cref="TableMetadata" />, which contains this column
+        /// </summary>
         public TableMetadata Table { get; internal set; }
 
+        /// <summary>
+        /// Implementation of <see cref="IVistitable{T}"/>. Only visits this
+        /// </summary>
         public void Accept(IDatabaseMetadataVisitor visitor)
         {
             visitor.Visit(this);
--- src/TCode.r2rml4net/RDB/TableMetadata.cs
+++ src/TCode.r2rml4net/RDB/TableMetadata.cs
@@ -5,12 +5,21 @@ using System.Text;
 
 namespace TCode.r2rml4net.RDB
 {
+    /// <summary>
+    /// Represents a table from a relational database
+    /// </summary>
     public class TableMetadata : IEnumerable<ColumnMetadata>, IVistitable<IDatabaseMetadataVisitor>
     {
         private readonly IList<ColumnMetadata> _columns = new List<ColumnMetadata>();
 
+        /// <summary>
+        /// Table name
+        /// </summary>
         public string Name { get; internal set; }
 
+        /// <summary>
+        /// Visits self and contained columns
+        /// </summary>
         public void Accept(IDatabaseMetadataVisitor visitor)
         {
             visitor.Visit(this);
@@ -19,17 +28,26 @@ namespace TCode.r2rml4net.RDB
                 column.Accept(visitor);
         }
 
+        /// <summary>
+        /// Implementation of <see cref="IEnumerable{T}"/>
+        /// </summary>
         public IEnumerator<ColumnMetadata> GetEnumerator()
         {
             return _columns.GetEnumerator();
         }
 
+        /// <summary>
+        /// Implementation of <see cref="IEnumerable{T}"/>
+        /// </summary>
         System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
         {
             return _columns.GetEnumerator();
         }
 
-        public void Add(ColumnMetadata column)
+        /// <summary>
+        /// Implemented to allow collection initialization
+        /// </summary>
+        internal void Add(ColumnMetadata column)
         {
             column.Table = this;
             _columns.Add(column);
