a9484cff432c8aa4c52c20bd3cc7741a37be461e
tpluscode <tomasz@t-code.pl>
2012-06-16 14:28:31 +0200

    missing file previously

---
+++ src/TCode.r2rml4net/RDB/ForeignKeyMetadata.cs
@@ -0,0 +1,29 @@
+namespace TCode.r2rml4net.RDB
+{
+    /// <summary>
+    /// Represents a foreign key
+    /// </summary>
+    public class ForeignKeyMetadata
+    {
+        /// <summary>
+        /// Gets the table containing this foreign key
+        /// </summary>
+        public string TableName { get; internal set; }
+        /// <summary>
+        /// Gets the foreign key column
+        /// </summary>
+        public string[] ForeignKeyColumns { get; internal set; }
+        /// <summary>
+        /// Get the referenced table
+        /// </summary>
+        public string ReferencedTableName { get; internal set; }
+        /// <summary>
+        /// Gets the referenced columns
+        /// </summary>
+        public string[] ReferencedColumns { get; internal set; }
+        /// <summary>
+        /// Returns true if the referenced columns do not form a primary key
+        /// </summary>
+        public bool IsCandidateKeyReference { get; internal set; }
+    }
+}
