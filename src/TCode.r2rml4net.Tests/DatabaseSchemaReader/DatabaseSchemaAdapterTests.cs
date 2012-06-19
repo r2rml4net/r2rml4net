f3989192d89f6f02f2589b4cfb9a6f86a7b06a73
tpluscode <tomasz@t-code.pl>
2012-06-19 15:45:32 +0200

    start DatabaseSchemaAdapter

---
+++ src/TCode.r2rml4net.Tests/DatabaseSchemaReader/DatabaseSchemaAdapterTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace TCode.r2rml4net.Tests.DatabaseSchemaReader
+{
+    [TestFixture]
+    public class DatabaseSchemaAdapterTests
+    {
+        
+    }
+}
