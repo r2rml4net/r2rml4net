8c2eed469a081c1caa81b084dade917ac73cbbb7
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-07-09 10:42:29 +0200

    test asserting rr:objectMap for ref object map

---
+++ src/TCode.r2rml4net.Mapping.Tests/Mapping/RefObjectMapConfigurationTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using VDS.RDF;
+
+namespace TCode.r2rml4net.Mapping.Tests.Mapping
+{
+    [TestFixture]
+    public class RefObjectMapConfigurationTests
+    {
+        RefObjectMapConfiguration _refObjectMap;
+
+        [SetUp]
+        public void Setup()
+        {
+            IGraph graph = new R2RMLConfiguration().R2RMLMappings;
+            IUriNode predicateObjectMap = graph.CreateUriNode(new Uri("http://test.example.com/PredicateObjectMap"));
+            _refObjectMap = new RefObjectMapConfiguration(predicateObjectMap, graph);
+        }
+
+        [Test]
+        public void CreatingAssertsObjectMapPropertyTriple()
+        {
+            _refObjectMap.R2RMLMappings.VerifyHasTripleWithBlankObject("http://test.example.com/PredicateObjectMap", UriConstants.RrObjectMapProperty);
+        }
+    }
+}
--- src/TCode.r2rml4net.Mapping/RefObjectMapConfiguration.cs
+++ src/TCode.r2rml4net.Mapping/RefObjectMapConfiguration.cs
@@ -9,9 +9,13 @@ namespace TCode.r2rml4net.Mapping
 {
     public class RefObjectMapConfiguration : BaseConfiguration, IRefObjectMapConfiguration, IRefObjectMap
     {
+        INode _refObjectMapNode;
+
         public RefObjectMapConfiguration(INode predicateObjectMapNode, IGraph mappings)
             : base(mappings)
         {
+            _refObjectMapNode = mappings.CreateBlankNode();
+            mappings.Assert(predicateObjectMapNode, mappings.CreateUriNode(R2RMLUris.RrObjectMapProperty), _refObjectMapNode);
         }
 
         #region Overrides of BaseConfiguration
