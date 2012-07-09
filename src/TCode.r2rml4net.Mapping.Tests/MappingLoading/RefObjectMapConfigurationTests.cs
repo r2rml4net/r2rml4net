72cc9939562ab9497e20bb6cbcbdadd7cccb7207
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-07-09 12:51:16 +0200

    imported patch init ref object map from existing graph

---
+++ src/TCode.r2rml4net.Mapping.Tests/MappingLoading/RefObjectMapConfigurationTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+using VDS.RDF;
+
+namespace TCode.r2rml4net.Mapping.Tests.MappingLoading
+{
+    [TestFixture]
+    public class RefObjectMapConfigurationTests
+    {
+        RefObjectMapConfiguration _refObjectMap;
+
+        [Test]
+        public void CanInitializeJoinConditions()
+        {
+            // given
+            IGraph graph = new Graph();
+            graph.LoadFromString(@"@prefix ex: <http://www.example.com/>.
+                                   @prefix rr: <http://www.w3.org/ns/r2rml#>.
+
+                                   ex:TriplesMap rr:predicateObjectMap [
+                                       rr:predicate ex:department;
+                                       rr:objectMap [
+                                           rr:parentTriplesMap ex:TriplesMap2;
+                                           rr:joinCondition [
+                                               rr:child ""DEPTNO"";
+                                               rr:parent ""ID"";
+                                           ];
+                                       ];
+                                   ].");
+
+            // when
+            _refObjectMap = new RefObjectMapConfiguration(graph.GetBlankNode("autos1"), graph.GetUriNode("ex:TriplesMap2"), graph);
+            _refObjectMap.RecursiveInitializeSubMapsFromCurrentGraph();
+
+            // then
+            Assert.AreEqual(1, _refObjectMap.JoinConditions.Count());
+            Assert.AreEqual("DEPTNO", _refObjectMap.JoinConditions.ElementAt(0).ChildColumn);
+            Assert.AreEqual("ID", _refObjectMap.JoinConditions.ElementAt(0).ParentColumn);
+        }
+    }
+}
