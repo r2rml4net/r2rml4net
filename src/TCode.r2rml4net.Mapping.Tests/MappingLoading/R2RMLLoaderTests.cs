4f91d547cbe06309878f1e426855e9b867e0cd8c
tpluscode <tomasz@t-code.pl>
2012-07-17 15:26:49 +0200

    pass tests for mapping loading

--- src/TCode.r2rml4net.Mapping.Tests/MappingLoading/R2RMLLoaderTests.cs
+++ src/TCode.r2rml4net.Mapping.Tests/MappingLoading/R2RMLLoaderTests.cs
@@ -6,6 +6,7 @@ using System.Reflection;
 using System.Text;
 using NUnit.Framework;
 using TCode.r2rml4net.RDF;
+using VDS.RDF;
 
 namespace TCode.r2rml4net.Mapping.Tests.MappingLoading
 {
@@ -13,28 +14,29 @@ namespace TCode.r2rml4net.Mapping.Tests.MappingLoading
     public class R2RMLLoaderTests
     {
         #region Simple Test GraphUri
-        private const string TestGraph =
-            @"@prefix rr: <http://www.w3.org/ns/r2rml#> .
-                                        @prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-                                        @base <http://mappingpedia.org/rdb2rdf/r2rml/tc/> .
+        private const string TestGraph = @"@prefix rr: <http://www.w3.org/ns/r2rml#> .
+@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
+@base <http://mappingpedia.org/rdb2rdf/r2rml/tc/> .
 
-                                        <StudentTriplesMap>
-                                             a rr:TriplesMap;
+<StudentTriplesMap>
+        a rr:TriplesMap;
     
-                                            rr:logicalTable [ rr:tableName ""Student""; ];
+    rr:logicalTable [ rr:tableName ""Student""; ];
 
-                                            rr:subjectMap 
-	                                        [ 
-	                                          rr:termType rr:BlankNode;  
-	                                          rr:class <http://example.com/Student>
-	                                        ];
+    rr:subjectMap 
+	[ 
+	    rr:termType rr:BlankNode;  
+	    rr:class <http://example.com/Student>
+	];
 
-                                            rr:predicateObjectMap
-                                            [ 
-                                              rr:predicateMap	[ rr:constant <http://example.com/Student#Name> ] ;
-                                              rr:objectMap		[ rr:column ""Name"" ]
-                                            ]
-                                            ."; 
+    rr:predicateObjectMap
+    [ 
+        rr:predicateMap	[ rr:constant <http://example.com/Student#Name> ] ;
+        rr:objectMap		[ rr:column ""Name"" ]
+    ]
+    .
+
+<CourseTriplesMap> a rr:TriplesMap .";
         #endregion
 
         [Test]
@@ -45,39 +47,44 @@ namespace TCode.r2rml4net.Mapping.Tests.MappingLoading
 
             // then
             Assert.IsNotNull(mappings);
+            Assert.AreEqual(2, mappings.TriplesMaps.Count());
+            Assert.AreEqual(
+                new Uri("http://mappingpedia.org/rdb2rdf/r2rml/tc/StudentTriplesMap"),
+                ((IUriNode)mappings.TriplesMaps.Cast<TriplesMapConfiguration>().ElementAt(0).ConfigurationNode).Uri);
+            Assert.AreEqual(
+                new Uri("http://mappingpedia.org/rdb2rdf/r2rml/tc/CourseTriplesMap"),
+                ((IUriNode)mappings.TriplesMaps.Cast<TriplesMapConfiguration>().ElementAt(1).ConfigurationNode).Uri);
         }
 
         [Test]
         public void CanLoadR2RMLFromStream()
         {
-            // given
-            using (Stream graphStream = new MemoryStream())
-            {
-                using (StreamWriter writer = new StreamWriter(graphStream))
-                {
-                    writer.Write(TestGraph);
-                    graphStream.Seek(0, SeekOrigin.Begin);
-                }
-
-                // when
-                IR2RML mappings = R2RMLLoader.Load(graphStream);
-
-                // then
-                Assert.IsNotNull(mappings);
-            }
-        }
-
-        [Test]
-        public void CanLoadComplexMapping()
-        {
             IR2RML mappings;
 
-            using (Stream turtle = Assembly.GetExecutingAssembly().GetManifestResourceStream("TCode.r2rml4net.Mapping.Tests.MappingLoading.ComplexTestGraph.ttl, TCode.r2rml4net.Mapping.Tests"))
+            // when
+            using (Stream turtle = Assembly.GetExecutingAssembly().GetManifestResourceStream("TCode.r2rml4net.Mapping.Tests.MappingLoading.ComplexTestGraph.ttl"))
             {
                 mappings = R2RMLLoader.Load(turtle);
             }
 
+            // then
+            Assert.IsNotNull(mappings);
             Assert.AreEqual(5, mappings.TriplesMaps.Count());
+            Assert.AreEqual(
+                new Uri("http://mappingpedia.org/rdb2rdf/r2rml/tc/StudentTriplesMap"),
+                ((IUriNode)mappings.TriplesMaps.Cast<TriplesMapConfiguration>().ElementAt(0).ConfigurationNode).Uri);
+            Assert.AreEqual(
+                new Uri("http://mappingpedia.org/rdb2rdf/r2rml/tc/TriplesMap2"),
+                ((IUriNode)mappings.TriplesMaps.Cast<TriplesMapConfiguration>().ElementAt(1).ConfigurationNode).Uri);
+            Assert.AreEqual(
+                new Uri("http://mappingpedia.org/rdb2rdf/r2rml/tc/TriplesMap1"),
+                ((IUriNode)mappings.TriplesMaps.Cast<TriplesMapConfiguration>().ElementAt(2).ConfigurationNode).Uri);
+            Assert.AreEqual(
+                new Uri("http://mappingpedia.org/rdb2rdf/r2rml/tc/ManyToManyTriplesMap"),
+                ((IUriNode)mappings.TriplesMaps.Cast<TriplesMapConfiguration>().ElementAt(3).ConfigurationNode).Uri);
+            Assert.AreEqual(
+                new Uri("http://mappingpedia.org/rdb2rdf/r2rml/tc/SimplerManyToManyTriplesMap"),
+                ((IUriNode)mappings.TriplesMaps.Cast<TriplesMapConfiguration>().ElementAt(4).ConfigurationNode).Uri);
         }
     }
 }
