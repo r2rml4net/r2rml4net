d6657cc8fc77b643e2769ab87d56ae9226310e42
tpluscode <tomasz@t-code.pl>
2012-07-19 20:51:34 +0200

    initial commit of ITriplesGenerator

---
+++ src/TCode.r2rml4net/TriplesGeneration/ITriplesGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace TCode.r2rml4net.TriplesGeneration
+{
+    /// <summary>
+    /// Interface for generating triples from R2RML mappings
+    /// </summary>
+    public interface ITriplesGenerator
+    {
+        /// <summary>
+        /// Generates triples from <paramref name="r2RML"/> mappings
+        /// </summary>
+        VDS.RDF.ITripleStore GenerateTriples(Mapping.IR2RML r2RML);
+    }
+}
