4bb0d99ed0d18dcd09029ceeb2e8989c88dd62e1
Tomasz Pluskiewicz <tpluscode@users.noreply.github.com>
2012-07-28 21:57:39 +0200

    Merge with default

---
+++ src/TCode.r2rml4net.Tests/RDB/DatabaseIdentifiersHelperTests.cs
@@ -0,0 +1,26 @@
+﻿using NUnit.Framework;
+using TCode.r2rml4net.RDB;
+
+namespace TCode.r2rml4net.Tests.RDB
+{
+    [TestFixture]
+    public class DatabaseIdentifiersHelperTests
+    {
+        [TestCase("[Column]", "Column")]
+        [TestCase("\"Column\"", "Column")]
+        [TestCase("`Column`", "Column")]
+        [TestCase("`Column with spaces and_underscored]", "Column with spaces and_underscored")]
+        [TestCase("\"Column with spaces and_underscored\"", "Column with spaces and_underscored")]
+        [TestCase("[Column with spaces and_underscored]", "Column with spaces and_underscored")]
+        public void EcsapesSimpleColumnNames(string inputName, string expectedName)
+        {
+            Assert.AreEqual(expectedName, DatabaseIdentifiersHelper.GetColumnNameUnquoted(inputName));
+        }
+
+        //[TestCase("Column with \"quoted value\"", @"""Column ""with quotes""")]
+        //public void EcsapesColumnNamesWithQuotes(string inputName, string expectedName)
+        //{
+        //    Assert.AreEqual(expectedName, DatabaseIdentifiersHelper.GetColumnNameUnquoted(inputName));
+        //}
+    }
+}
