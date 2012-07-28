bc3cb88cd57f911189d07c77f275b97ba137f9f6
tpluscode <tomasz@t-code.pl>
2012-07-28 21:56:52 +0200

    Added DatabaseIdentifiersHelper

---
+++ src/TCode.r2rml4net/RDB/DatabaseIdentifiersHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.RegularExpressions;
+
+namespace TCode.r2rml4net.RDB
+{
+    static class DatabaseIdentifiersHelper
+    {
+        private static readonly Regex ColumnNameRegex = new Regex(@"^[\""`'\[]+([_ a-zA-Z0-9]+)[\""`'\]]+$");
+
+        internal static string GetColumnNameUnquoted(string columnName)
+        {
+            return ColumnNameRegex.Replace(columnName, "$1");
+        }
+    }
+}
