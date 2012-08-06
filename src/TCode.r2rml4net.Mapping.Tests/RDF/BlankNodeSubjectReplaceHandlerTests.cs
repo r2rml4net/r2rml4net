f363c68711f990f4a05b2293c62eaaba71dc8203
tpluscode <tomasz@t-code.pl>
2012-08-06 18:40:36 +0200

    BlankNodeSubjectReplaceHandler also replaced blank node objects

--- src/TCode.r2rml4net.Mapping.Tests/RDF/BlankNodeSubjectReplaceHandlerTests.cs
+++ src/TCode.r2rml4net.Mapping.Tests/RDF/BlankNodeSubjectReplaceHandlerTests.cs
@@ -21,7 +21,7 @@ namespace TCode.r2rml4net.Mapping.Tests.RDF
         }
 
         [Test]
-        public void ReplacesBlankNodeOnce()
+        public void ReplacesBlankNodeSubjectOnce()
         {
             // given
             var newBlankNode = MockNode<IBlankNode>();
@@ -64,6 +64,30 @@ namespace TCode.r2rml4net.Mapping.Tests.RDF
             // then
             Assert.AreSame(subj, triple.Subject);
             Assert.AreSame(subj1, triple1.Subject);
+        }
+
+        [Test]
+        public void AlsoReplacesBlankNodeObjects()
+        {
+            // given
+            var newBlankNode = MockNode<IBlankNode>();
+            var blankNode = new Mock<IBlankNode>();
+            blankNode.Setup(b => b.InternalID).Returns("some invalid identifier");
+            IList<Triple> triples = new List<Triple>();
+            Triple triple = new Triple(blankNode.Object, MockNode<IUriNode>(), MockNode<ILiteralNode>());
+            Triple triple2 = new Triple(MockNode<IUriNode>(), MockNode<IUriNode>(), blankNode.Object);
+            _decoratedHandler.Setup(h => h.HandleTriple(It.IsAny<Triple>()))
+                             .Callback((Triple t) => triples.Add(t))
+                             .Returns(true);
+            _decoratedHandler.Setup(h => h.CreateBlankNode()).Returns(newBlankNode);
+
+            // when
+            _handler.HandleTriple(triple);
+            _handler.HandleTriple(triple2);
+
+            // then
+            _decoratedHandler.Verify(h => h.CreateBlankNode(), Times.Once()); 
+            Assert.AreEqual(triples[0].Subject, triples[1].Object);
         }
 
         private TNode MockNode<TNode>() where TNode : class, INode
