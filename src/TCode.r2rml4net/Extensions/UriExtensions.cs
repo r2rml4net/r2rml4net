e48072f3bc8de901e25d9a31d470148a79548cec
tpluscode <tomasz@t-code.pl>
2012-08-22 18:33:04 +0200

    extracted the UriExtensions class

---
+++ src/TCode.r2rml4net/Extensions/UriExtensions.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace TCode.r2rml4net.Extensions
+{
+    public static class UriExtensions
+    {
+        /// <summary>
+        /// A pretty dirty workaround to have slahses unescaped to avoid relative
+        /// Uri resolution and simmilar malfunctions
+        /// </summary>
+        /// <remarks>See http://stackoverflow.com/questions/2320533/system-net-uri-with-urlencoded-characters</remarks>
+        public static void LeaveDotsAndSlashesEscaped(this System.Uri uri)
+        {
+            const int unEscapeDotsAndSlashes = 0x2000000;
+            if (uri == null)
+            {
+                throw new ArgumentNullException("uri");
+            }
+
+            FieldInfo fieldInfo = uri.GetType().GetField("m_Syntax", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fieldInfo == null)
+            {
+                throw new MissingFieldException("'m_Syntax' field not found");
+            }
+            object uriParser = fieldInfo.GetValue(uri);
+
+            fieldInfo = typeof(UriParser).GetField("m_Flags", BindingFlags.Instance | BindingFlags.NonPublic);
+            if (fieldInfo == null)
+            {
+                throw new MissingFieldException("'m_Flags' field not found");
+            }
+            object uriSyntaxFlags = fieldInfo.GetValue(uriParser);
+
+            // Clear the flag that we don't want
+            uriSyntaxFlags = (int)uriSyntaxFlags & ~unEscapeDotsAndSlashes;
+
+            fieldInfo.SetValue(uriParser, uriSyntaxFlags);
+        }
+    }
+}
