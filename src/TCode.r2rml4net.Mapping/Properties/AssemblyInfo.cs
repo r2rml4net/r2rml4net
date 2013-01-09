41be4e470dc9a934bf716d3d289fb216898d0a2f
tpluscode <tomasz@t-code.pl>
2013-01-09 11:50:24 +0100

    remove nunit runner and change tests to dll

--- src/TCode.r2rml4net.Mapping/Properties/AssemblyInfo.cs
+++ src/TCode.r2rml4net.Mapping/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("TCode.r2rml4net.Mapping")]
 [assembly: AssemblyCopyright("Copyright © Tomasz Pluskiewicz 2012")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.3.0.4653")]
+[assembly: AssemblyVersion("0.3.0.4757")]
 [assembly: AssemblyFileVersion("0.3.0.0")]
 [assembly: InternalsVisibleTo("TCode.r2rml4net.Mapping.Tests")]
 
