b3a6fa7a6471097a0bdbdac1f42884df06364465
tpluscode <tomasz@t-code.pl>
2014-10-20 20:43:53 +0200

    ignore version files

--- src/TCode.r2rml4net.Mapping.Tests/Properties/VersionAssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-// This is a generated file.  Do not commit it to version control and do not modify it.
-using System.Reflection;
-[assembly: AssemblyVersion("0.3.76.0")]
-[assembly: AssemblyFileVersion("0.3.76.0")]
-[assembly: AssemblyInformationalVersion("0.3.76.0")]
