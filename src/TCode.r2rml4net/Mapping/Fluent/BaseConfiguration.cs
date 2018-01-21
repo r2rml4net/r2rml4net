dcd20cb037efc2c9f6c77fee1389fc5cf323a3f8
Miloš Chaloupka <chaloupkamilos@hotmail.com>
2018-01-21 18:39:27 +0100

    Fixes #24. The triplesmap reference is added to IMapBase.

--- src/TCode.r2rml4net/Mapping/Fluent/BaseConfiguration.cs
+++ src/TCode.r2rml4net/Mapping/Fluent/BaseConfiguration.cs
@@ -2,35 +2,35 @@
 // Copyright (C) 2012-2014 Tomasz Pluskiewicz
 // http://r2rml.net/
 // r2rml@t-code.pl
-// 	
+//
 // ------------------------------------------------------------------------
-// 	
+//
 // This file is part of r2rml4net.
-// 	
+//
 // Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal 
-// in the Software without restriction, including without limitation the rights 
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 
-// copies of the Software, and to permit persons to whom the Software is 
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in all 
+//
+// The above copyright notice and this permission notice shall be included in all
 // copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS 
-// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE 
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+// OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+// FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE
 // OR OTHER DEALINGS IN THE SOFTWARE.
-// 	
+//
 // ------------------------------------------------------------------------
-// 
+//
 // r2rml4net may alternatively be used under the LGPL licence
-// 
+//
 // http://www.gnu.org/licenses/lgpl.html
-// 
+//
 // If these licenses are not suitable for your intended use please contact
 // us at the above stated email address to discuss alternative
 // terms.
@@ -51,7 +51,7 @@ namespace TCode.r2rml4net.Mapping.Fluent
     /// </summary>
     public abstract class BaseConfiguration : IMapBase
     {
-        private static readonly string ShortcutSubmapsReplaceSparql = Resource.AsString("Queries.ReplaceShortcuts.rq"); 
+        private static readonly string ShortcutSubmapsReplaceSparql = Resource.AsString("Queries.ReplaceShortcuts.rq");
         private readonly INode _node;
         private readonly ITriplesMapConfiguration _triplesMap;
 
@@ -121,11 +121,21 @@ namespace TCode.r2rml4net.Mapping.Fluent
         protected internal IGraph R2RMLMappings { get; private set; }
 
         /// <summary>
-        /// Gets the parent <see cref="ITriplesMapConfiguration"/> containing this map
+        /// Gets the parent <see cref="ITriplesMapConfiguration" /> containing this map
         /// </summary>
+        /// <value>The triples map.</value>
         protected internal virtual ITriplesMapConfiguration TriplesMap
         {
             get { return _triplesMap; }
+        }
+
+        /// <summary>
+        /// Gets the parent <see cref="ITriplesMapConfiguration" /> containing this map
+        /// </summary>
+        /// <value>The triples map.</value>
+        ITriplesMap IMapBase.TriplesMap
+        {
+            get { return TriplesMap; }
         }
 
         /// <summary>
@@ -146,7 +156,7 @@ namespace TCode.r2rml4net.Mapping.Fluent
         }
 
         /// <summary>
-        /// Implemented in child classes should create submaps and for each of the run the 
+        /// Implemented in child classes should create submaps and for each of the run the
         /// <see cref="BaseConfiguration.RecursiveInitializeSubMapsFromCurrentGraph"/> method
         /// </summary>
         protected abstract void InitializeSubMapsFromCurrentGraph();
